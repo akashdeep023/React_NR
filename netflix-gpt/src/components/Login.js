dc284ac39c6ce19eadfb99de3b4b05ea26c44826
akashdeep023 <ad3500476@gmail.com>
2024-02-23 12:45:05 +0530

    Validation SignUp/SignIn Form

--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
 import Header from "./Header";
 import { useState } from "react";
+import {
+	checkValidSignInFrom,
+	checkValidSignUpFrom,
+} from "../utils/validate.js";
 
 const Login = () => {
 	const [isSignIn, setIsSignIn] = useState(true);
+	const [errorMsg, setErrorMsg] = useState(null);
+	const fullName = useRef(null);
+	const email = useRef(null);
+	const password = useRef(null);
 	const toggleForm = () => {
+		// Toggle SignUp/SignIn
 		setIsSignIn(!isSignIn);
+	};
+	const handleSignInFormValidation = () => {
+		const message = checkValidSignInFrom(
+			email.current.value,
+			password.current.value
+		); // Validate Form
+		setErrorMsg(message); // Set error message
+	};
+	const handleSignUpFormValidation = () => {
+		const message = checkValidSignUpFrom(
+			fullName.current.value,
+			email.current.value,
+			password.current.value,
+		); // Validate Form
+		console.log(message);
+		setErrorMsg(message); // Set error message
 	};
 	return (
 		<div className="bg-black/50">
@@ -18,35 +43,50 @@ const Login = () => {
 				/>
 			</div>
 			<div className="w-full flex justify-center ">
-				<form className="absolute px-16 py-12 text-white font-bold w-[450px] bg-black/60 top-20 left-auto rounded-md flex flex-col gap-4">
+				<form
+					className="absolute px-16 py-12 text-white font-bold w-[450px] bg-black/60 top-20 left-auto rounded-md flex flex-col gap-4"
+					onSubmit={(event) => event.preventDefault()}
+				>
 					<h1 className="rounded-sm text-4xl font-bold text-white mb-4">
 						{isSignIn ? "Sign In" : "Sign Up"}
 					</h1>
 					{!isSignIn && (
 						<input
+							ref={fullName}
 							type="text"
 							placeholder="Full Name"
 							className="rounded-md w-full h-full p-4 font-semibold bg-black/50 border border-gray-200/50"
 						/>
 					)}
 					<input
+						ref={email}
 						type="text"
 						placeholder="Email Address"
 						className="rounded-md w-full h-full p-4 font-semibold bg-black/50 border border-gray-200/50"
 					/>
 					<input
+						ref={password}
 						type="password"
 						placeholder="Password"
 						className="rounded-md w-full h-full p-4 font-semibold bg-black/50 border border-gray-200/50"
 					/>
-					<button className="rounded-sm w-full h-full p-2 my-4 bg-red-600 hover:bg-red-700 active:bg-red-900">
+					{errorMsg && <p className="text-red-500">{errorMsg}</p>}
+					<button
+						type="submit"
+						className="rounded-sm w-full h-full p-2 my-4 bg-red-600 hover:bg-red-700 active:bg-red-900"
+						onClick={
+							isSignIn
+								? handleSignInFormValidation
+								: handleSignUpFormValidation
+						}
+					>
 						{isSignIn ? "Sign In" : "Sign Up"}
-                    </button>
-                    {isSignIn &&
-					<p className="text-center font-semibold">
-						Forgot password?
-					</p>
-                    }
+					</button>
+					{isSignIn && (
+						<p className="text-center font-semibold">
+							Forgot password?
+						</p>
+					)}
 					<p className=" font-normal text-gray-300/80">
 						{isSignIn ? "New to Netflix?" : "Already registered?"}{" "}
 						<b
---
+++ netflix-gpt/src/utils/validate.js
@@ -0,0 +1,16 @@
+export const checkValidSignInFrom = (email, password) => {
+    const isEmailValid = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password)
+    if (!isEmailValid) return "Please enter a valid email";
+    if (!isPasswordValid) return "Please enter a valid password";
+    return null;
+}
+export const checkValidSignUpFrom = (name, email, password) => {
+    const isNameValid = /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/.test(name);
+    const isEmailValid = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password)
+    if (!isNameValid) return "Please enter a valid Name";
+    if (!isEmailValid) return "Please enter a valid Email";
+    if (!isPasswordValid) return "Please enter a valid Password";
+    return null;
+}
